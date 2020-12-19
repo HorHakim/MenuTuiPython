e33c474f26417342676e64d0642fb021b68c245f
HorHakim <horairyhakim@gmail.com>
2020-12-19 23:40:44 +0100

    Rajout d'un curseur

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ email : hakim.horairy@telecom-sudparis.eu
 Tel : 07 69 22 52 55
 """
 from Frame import *
-from Cursor import *
+# from Cursor import *
 
 frame = Frame(frameID=1)
-frame.ShowGraphicalGrid()
-cursor = Cursor(frame=frame)
+
+while True :
+	frame.ShowGraphicalGrid()
+	frame.command()
+	frame.ShowGraphicalGrid()
