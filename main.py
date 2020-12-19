7fbf51146ff9a064ea06d2619a02f3c0ff70f16a
HorHakim <horairyhakim@gmail.com>
2020-12-19 16:25:34 +0100

    ajout de la classe Cursor

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ email : hakim.horairy@telecom-sudparis.eu
 Tel : 07 69 22 52 55
 """
 from Frame import *
+from Cursor import *
 
 frame = Frame(frameID=1)
 frame.ShowGraphicalGrid()
+cursor = Cursor(frame=frame)
